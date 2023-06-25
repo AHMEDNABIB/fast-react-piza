57303fecb8735e76622e5624587f734de30a842e
AHMED NABIB <nabibpallab@gmail.com>
2023-06-26 00:36:45 +0600

    menu data fetching complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Cart from "./features/cart/Cart";
-import Menu from './features/menu/Menu';
+import Menu, {loader as menuLoader} from './features/menu/Menu';
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
 import Home from "./ui/Home";
@@ -20,7 +20,8 @@ const router = createBrowserRouter([
 			},
 			{
 				path: "/menu",
-				element: <Menu />,
+        element: <Menu />,
+        loader: menuLoader
 				
 			},
 			{ path: "/cart", element: <Cart /> },
