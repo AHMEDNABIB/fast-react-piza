7c36cbdce954d1fe7b9415fc2982a37a1b127819
AHMED NABIB <nabibpallab@gmail.com>
2023-06-27 00:45:24 +0600

    create order complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Cart from "./features/cart/Cart";
 import Menu, {loader as menuLoader} from './features/menu/Menu';
-import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import CreateOrder, {
+	action as createOrderAction,
+} from "./features/order/CreateOrder";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import Home from "./ui/Home";
 import Error from "./ui/Error";
 import AppLayout from "./ui/Applayout";
@@ -30,12 +32,15 @@ const router = createBrowserRouter([
 			{ path: "/cart", element: <Cart /> },
 			{
 				path: "/order/new",
-				element: <CreateOrder />,
+        element: <CreateOrder />,
+        action: createOrderAction
 				
 			},
 			{
 				path: "/order/:orderId",
-				element: <Order />,
+        element: <Order />,
+        loader: orderLoader,
+        errorElement:<Error/>
 				
 			},
 		],
