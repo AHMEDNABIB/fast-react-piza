1e9d8a38256fd7133d78b61f7cb7f1c5307a1eee
AHMED NABIB <nabibpallab@gmail.com>
2023-07-06 16:47:28 +0600

    cart display and delete button to cart and menu item complete

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,8 +1,27 @@
 import { formatCurrency } from "./../../utils/helpers";
 import Button from './../../ui/Button';
+import { useDispatch, useSelector } from "react-redux";
+import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
+import { DeleteItem } from "../cart/DeleteItem";
 
 function MenuItem({ pizza }) {
+  const dispatch = useDispatch()
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+    const currentQuantity = useSelector(getCurrentQuantityById(id));
+    const isInCart = currentQuantity > 0;
+
+ function handleAddToCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+    dispatch(addItem(newItem));
+  }
+
 
   return (
     <li className="flex gap-4 py-2">
@@ -11,7 +30,7 @@ function MenuItem({ pizza }) {
         alt={name}
         className={`h-24 ${soldOut ? "opacity-70 grayscale" : ""}`}
       />
-      <div className="flex grow flex-col mt-0.5">
+      <div className="mt-0.5 flex grow flex-col">
         <p className="font-medium">{name}</p>
         <p className="text-sm capitalize italic text-stone-500">
           {ingredients.join(", ")}
@@ -24,8 +43,13 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
+          {isInCart && <DeleteItem pizzaId={id} />}
 
-          <Button type='small'>add to cart</Button>
+          {!soldOut && !isInCart && (
+            <Button type="small" onClick={handleAddToCart}>
+              add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
